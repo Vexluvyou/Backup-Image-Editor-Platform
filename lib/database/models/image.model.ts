eaec1ee066a2d09b6a58c1ca3459acadfc370aa6
Thy Chamroeunpiseth <seththy.sorry123@gmail.com>
2025-04-24 16:30:00 +0700

    fix error

--- lib/database/models/image.model.ts
+++ lib/database/models/image.model.ts
@@ -1,43 +1,42 @@
-import { Document, model, models, Schema } from "mongoose";
+import { Document, Schema, model, models } from "mongoose";
 
 export interface IImage extends Document {
   title: string;
   transformationType: string;
   publicId: string;
-  secureUrl: string;
+  secureURL: string; 
   width?: number;
   height?: number;
-  config?: object;
-  transformationUrl?: string;
+  config?: object; 
+  transformationUrl?: string; 
   aspectRatio?: string;
   color?: string;
   prompt?: string;
-  author?: {
+  author: {
     _id: string;
     firstName: string;
     lastName: string;
   }
   createdAt?: Date;
-  updateAt?: Date;
+  updatedAt?: Date;
 }
 
-
 const ImageSchema = new Schema({
-    title: { type: String, require: true },
-    transformationType: {type: String, require: true},
-    publicId: { type: String, require: true },
-    secureUrl: { type: URL, require: true },
-    width: { type: Number },
-    height: { type: Number },
-    config: { type: Object },
-    transformationUrl: { type: URL },
-    aspectRatio: { ype: String },
-    color: { type: String },
-    prompt: { type: String },
-    author: { type: Schema.Types.ObjectId, ref: 'User' },
-    createdAt: { type: Date, default: Date.now },
-    updateAt: { type: Date, default: Date.now },
-})
+  title: { type: String, required: true },
+  transformationType: { type: String, required: true },
+  publicId: { type: String, required: true },
+  secureURL: { type: String, required: true },
+  width: { type: Number },
+  height: { type: Number },
+  config: { type: Object },
+  transformationUrl: { type: String },
+  aspectRatio: { type: String },
+  color: { type: String },
+  prompt: { type: String },
+  author: { type: Schema.Types.ObjectId, ref: 'User' },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
+});
 
 const Image = models?.Image || model('Image', ImageSchema);
 
